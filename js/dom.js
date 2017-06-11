3f330daa750bf7546ecc59065a00a4aaaad6d045
Marcin Mika <marcinmika@hotmail.com>
2017-06-11 10:01:24 +0200

    tworzenie zmienych w js na obiektach z html

---
+++ js/dom.js
@@ -0,0 +1,28 @@
+/*wyświetlanie */
+
+'use strict';
+//console.log(window);
+//console.log(window.innerHeight);
+//console.log(window.outerHeight);
+//
+//console.log(document);
+//console.log(document.element);
+//console.log(document.head);
+//console.log(document.body);
+
+//***************************************************************
+/*tworzenie zmeinnych na elementach obiektow html*/
+var parFirst = document.getElementById('parFirst');
+console.log(parFirst);
+
+var link = document.getElementsByClassName('superlink');
+console.log(link);
+
+var linkPoTagu = document.getElementsByTagName('a');
+console.log(linkPoTagu);
+
+var divPoID = document.getElementById('parSecond');
+console.log(divPoID);
+//***************************************************************
+/*NODES węzły*/
+
